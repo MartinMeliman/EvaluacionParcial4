a2acdf1d4dd35f7f83296eb336ee1665ee8ec618
MartinMeliman <ma.meliman@duocuc.cl>
2025-06-27 20:33:29 -0400

    creación menú
    creación menú para la reserva

---
+++ Meliman_Martin_Ejercicio.py
@@ -0,0 +1,11 @@
+
+reservas = {}
+
+max_reservas = 20
+
+def menu_reserva():
+    print("\nTOTEM AUTOATENCIÓN RESERVA STRIKE")
+    print("1.- Reservar zapatillas")
+    print("2.- Buscar zapatillas reservadas")
+    print("3.- Ver stock de reservas")
+    print("4.- Salir")
