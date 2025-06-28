8e32aaa0e5bef08e21c3e5e4802622a58ce39c30
MartinMeliman <ma.meliman@duocuc.cl>
2025-06-27 20:34:13 -0400

    Reserva zapatillas
    Crear función para reservar zapatillas

--- Meliman_Martin_Ejercicio.py
+++ Meliman_Martin_Ejercicio.py
@@ -9,3 +9,24 @@ def menu_reserva():
     print("2.- Buscar zapatillas reservadas")
     print("3.- Ver stock de reservas")
     print("4.- Salir")
+
+    
+def reservar_zapatillas():
+    print("---- Reservar Zapatillas ----")
+    if sum(reservas.values()) >= max_reservas:
+        print("Error, stock completo")
+        return
+    
+    nombre = input("Nombre del comprador: ").strip
+    
+    if nombre in reservas:
+        print("Error nombre ya registrado en la reserva.")
+        return
+    
+    clave = input("Digite la palabra secreta para confirmar la reserva: ")
+
+    if clave != "EstoyEnListaDeReserva":
+        print("Error: palabra clave incorrecta. Reserva no realizada.")
+        return
+    reservas[nombre] = 1
+    print(f"Reserva realizada exitosamente para {nombre}")
